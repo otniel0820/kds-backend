c6ecd36c4f0ea4cb65ca256d5e54255456bd3e60
Otniel Lascano <otniel.lascano@ikualo.com>
2026-02-07 14:09:34 +0100

    feat: update EnviromentSchema to include GLOVO credentials and export additional schemas

--- src/common/schemas/enviroment.schema.ts
+++ src/common/schemas/enviroment.schema.ts
@@ -4,6 +4,8 @@ export const EnviromentSchema = z.object({
   NEST_NODE_ENV: z.enum(['LOCAL', 'DEVELOPMENT', 'SANDBOX', 'PRODUCTION']),
   NEST_PORT: z.string().min(1),
   NEST_MONGO_URL: z.string(),
+  NEST_USER_NAME_GLOVO: z.string(),
+  NEST_PASSWORD_GLOVO: z.string(),
 });
 
 export type IEnviroment = z.infer<typeof EnviromentSchema>;
--- src/common/schemas/index.ts
+++ src/common/schemas/index.ts
@@ -1,1 +1,3 @@
 export * from './enviroment.schema';
+export * from './error.schema';
+export * from './response.schema';
