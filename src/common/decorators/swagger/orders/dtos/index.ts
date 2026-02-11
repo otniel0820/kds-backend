189cda11c8179cd29665439f11ea63ea5625775b
Otniel Lascano <otniel.lascano@ikualo.com>
2026-02-11 16:02:58 +0100

    chore: remove deprecated DTO and swagger decorators

--- src/common/decorators/swagger/orders/dtos/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ingest-order.swagger.dto';
-export * from './order-details.swagger.dto';
-export * from './order-list.swagger.dto';
-export * from './update-order-status.swagger.dto';
--- src/common/decorators/swagger/orders/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './api-ingest-order.decorator';
-export * from './api-list-order.decorator';
-export * from './api-order-details.decorator';
-export * from './api-update-order-status.decorator';
